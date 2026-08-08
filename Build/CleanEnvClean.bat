e293b4048148b6cddec2b80dab81722cbcfe0eef
cloudcheng <cloudcheng@tencent.com>
2026-08-08 17:54:56 +0800

    编译Code工程支持

---
+++ Build/CleanEnvClean.bat
@@ -0,0 +1,16 @@
+@echo off
+rem See CleanEnvBuild.bat for full description.
+set "MSBuildSDKsPath="
+set "MSBuildExtensionsPath="
+set "MSBuildToolsPath="
+set "MSBuildToolsPath32="
+set "MSBuildToolsPath64="
+set "MSBuildToolsRoot="
+set "MSBuildFrameworkToolsPath="
+set "MSBuildFrameworkToolsPath32="
+set "MSBuildFrameworkToolsPath64="
+set "MSBuildLoadMicrosoftTargetsReadOnly="
+set "MSBuildEnableWorkloadResolver="
+
+call "E:\UnrealEngine\Engine\Build\BatchFiles\Clean.bat" %*
+exit /B %ERRORLEVEL%
